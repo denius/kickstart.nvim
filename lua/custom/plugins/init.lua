936aafb105c5a7dfa86cad9766d6e8ea362c1864
Denis Telnov <denis.telnov@gmail.com>
2024-12-15 05:15:12 +0300

    Disable Autoformat

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -4,6 +4,11 @@
 -- See the kickstart.nvim README for more information
 return {
 
+  { -- Autoformat
+    'stevearc/conform.nvim',
+    -- by denis: DISABLE!
+    enabled = false,
+  },
 
   -- by denis
   {
