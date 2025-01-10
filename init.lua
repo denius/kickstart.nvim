91f29870de82e4e6ece24b7b473e384048a0850c
Denis Telnov <denis.telnov@gmail.com>
2025-01-10 23:51:23 +0300

    Improve up/down keys maps in Insert mode to suppress showing :call'ed command

--- init.lua
+++ init.lua
@@ -280,11 +280,14 @@ vim.keymap.set('n', '<LocalLeader>w', ':set wrap!<cr>', { silent = true }) -- li
 -- UP/DOWN will go through screen lines even for long lines
 -- by https://stackoverflow.com/questions/5478933/vim-move-around-quickly-inside-of-long-line
 -- vim.keymap.set('n', '<Up>', ':call execute("normal gk")<CR>', { silent = true })
-vim.keymap.set('n', '<Up>', 'gk', { silent = false })
-vim.keymap.set('i', '<Up>', '<C-o>:call execute("normal gk")<CR>', { silent = true })
--- vim.keymap.set('n', '<Down>', ':call execute("normal gj")<CR>', { silent = true })
-vim.keymap.set('n', '<Down>', 'gj', { silent = false })
-vim.keymap.set('i', '<Down>', '<C-o>:call execute("normal gj")<CR>', { silent = true })
+vim.keymap.set('n', '<Up>', 'gk', { silent = true })
+vim.keymap.set('i', '<Up>', function()
+                              vim.api.nvim_command('normal! gk')
+                            end, { silent = true, expr = false })
+vim.keymap.set('n', '<Down>', 'gj', { silent = true })
+vim.keymap.set('i', '<Down>', function()
+                                vim.api.nvim_command('normal! gj')
+                              end, { silent = true, expr = false })
 
 -- by denis: Remap for dealing with word wrap
 vim.keymap.set('n', 'k', 'v:count == 0 ? "gk" : "k"', { expr = true, silent = true })
